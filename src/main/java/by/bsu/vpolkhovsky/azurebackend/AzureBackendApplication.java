037976981582a46804d824f214ad4eb7affeeb84
vladpolkhovsky <vlad.polkhovsky@gmail.com>
2022-05-11 23:14:16 +0300

    initial commit

--- src/main/java/by/bsu/vpolkhovsky/azurebackend/AzureBackendApplication.java
+++ src/main/java/by/bsu/vpolkhovsky/azurebackend/AzureBackendApplication.java
@@ -2,12 +2,26 @@ package by.bsu.vpolkhovsky.azurebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class AzureBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOriginPatterns("*");
+			}
+		};
 	}
 
 }
